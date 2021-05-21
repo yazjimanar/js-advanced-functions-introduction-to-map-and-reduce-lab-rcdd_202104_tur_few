489cf346f3ecda44aea011c8801c6312d4f9df88
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-21 11:19:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Your code here
+const mapToNegativize = (sourceArray) => {
+                let arr = [];
+                for (let i = 0; i < sourceArray.length; i++){
+                                arr.push((-1 * sourceArray[i]));
+                }
+                return arr;
+};
+
+const mapToNoChange = (sourceArray) => {
+                let arr = [...sourceArray];
+                return arr;
+};
+
+const mapToDouble = (sourceArray) => {
+                let arr = [];
+                for (let i = 0; i < sourceArray.length; i++){
+                                arr.push((sourceArray[i] * 2));
+                }
+                return arr;
+};
+
+
+const mapToSquare = (sourceArray) => {
+                let arr = [];
+                for (let i = 0; i < sourceArray.length; i++){
+                                arr.push((sourceArray[i] ** 2));
+                }
+                return arr;
+};
+
+
+const reduceToTotal = (sourceArray, startingPoint) => {
+                let total = 0;
+                if(startingPoint) {
+                                total += startingPoint;
+                                for(let i = 0; i < sourceArray.length; i++) total += sourceArray[i];
+                }
+                else for(let i = 0; i < sourceArray.length; i++) total += sourceArray[i];
+                return total;
+};
+
+const reduceToAllTrue = (sourceArray) => {
+                for(let i = 0; i < sourceArray.length; i++) if (!sourceArray[i]) return false;
+                return true;
+};
+
+const reduceToAnyTrue = (sourceArray) => {
+                for(let i = 0; i < sourceArray.length; i++) if (sourceArray[i]) return true;
+                return false;
+};
