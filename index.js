47851981f9a13000f9c11d1ad448b15a2f7a2873
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-21 11:13:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code here
+const mapToNegativize = (sourceArray) => {
+                let arr = [];
+                for (let i = 0; i < sourceArray.length; i++){
+                                arr.push((-1 * sourceArray[i]));
+                }
+                return arr;
+};
